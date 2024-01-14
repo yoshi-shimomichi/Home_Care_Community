889b71b64e7584c1ae53a8163575cd03211dfd2b
下道義弘 <shimotsumichiyoshihiro@shimotsumichiyoshihironoMacBook-Air.local>
2024-01-14 11:22:00 +0900

    Userテーブルの作成、JQuery,Bootstrapの動作確認

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+// Javascriptの設定
+//= require jquery3
+//= require popper
+//= require bootstrap
+
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Javascript確認用
+$(function(){
+    alert("jQuery 動いた！")
+  })
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "users#index"
+
+  resources :users, only: %i[index new create edit update]
+
 end
